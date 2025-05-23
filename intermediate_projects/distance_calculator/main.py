4f0c516fefbce29a17c2b01be7ab82a86b23bb8c
Tia Webster <tiaa@hotmail.com.au>
2025-05-23 10:59:15 +1000

    click save

---
+++ intermediate_projects/distance_calculator/main.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+
+
+@dataclass
+class Coordinates: 
+    latitude: float
+    longitude: float
+    
+    def coordinates(self):
+        return self.latitude, self.longitude
+    
+
+def get_coordinates(address: str) -> Coordinates | None: 
+    geolocator = Nominatim(user_agent='distance_calculator')
+    location = geolocator.geocode(address)
+    
+    if location: 
+        return Coordinates(latitude=location.latitude, longitude=location.longitude)
+    
+    
+def main(): 
+    home_address = '123 Main St, Sydney, Australia'
+    print(f'Home address: {home_address}')
+    
+    print(get_coordinates(home_address))
