7f8e72c9164cd64687a83cc33a72a199000c86ba
Tia Webster <tiaa@hotmail.com.au>
2025-07-11 12:12:59 +1000

    print nicely

--- bigger_projects/weather_app/weather_api.py
+++ bigger_projects/weather_app/weather_api.py
@@ -1,5 +1,7 @@
+from datetime import datetime as dt
 import json
 import requests
+from model import Weather
 
 API_KEY = 'a40df88d3825d8b7dc95106c639a4cdb'
 BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast?'
@@ -17,5 +19,29 @@ def get_weather(city_name: str, mock: bool = True) -> dict:
     
     return data
 
+
+def get_weather_details(weather: dict) -> list[Weather]:
+    days = weather.get('list')
+    
+    if not days: 
+        raise Exception("Problem with json: {weather}")
+    
+    list_of_weather = []
+    
+    for day in days: 
+        w = Weather(date=dt.fromtimestamp(day.get('dt')),
+                    details=(details := day.get('main')),
+                    temp=details.get('temp'),
+                    weather=(weather := day.get('weather')),
+                    description=weather[0].get('description'))
+        list_of_weather.append(w)
+        
+    return list_of_weather
+                    
+
 if __name__ == '__main__':
-    print(get_weather('tokyo', mock=False))
+    current_weather = get_weather('tokyo', mock=False)
+    weather = get_weather_details(current_weather)
+
+    for w in weather: 
+        print(w)
